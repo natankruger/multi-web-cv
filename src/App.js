c2e403ddcaa363ec3701497853da76369474f320
Natan Kruger <natan.kruger@zygotecnologia.com>
2020-05-20 15:32:50 -0300

    Fixing routes and authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Main from './pages/main';
 import Login from './pages/login';
@@ -8,30 +8,45 @@ import Register from './pages/register';
 
 import { UserContext } from './services/auth';
 
-
 import 'bootstrap';
 import './styles/application.scss';
 
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const auth = useContext(UserContext);
+
+  return <Route {...rest}
+         render={ props => !!auth.user ? (
+          <Component { ...props } />
+         ) : (
+          <Redirect to={ { pathname: '/login', state: { from: props.location } } } />
+         ) }
+  />
+};
+
+const AuthRoute = ({ component: Component, ...rest }) => {
+  const auth = useContext(UserContext);
+
+  return <Route {...rest}
+         render={ props => !auth.user ? (
+          <Component { ...props } />
+         ) : (
+          <Redirect to={ { pathname: '/', state: { from: props.location } } } />
+         ) }
+  />
+};
+
 function App() {
   const { t } = useTranslation();
-  const auth = useContext(UserContext);
-  console.log(auth);
 
   return (
-    <Router>
-      { !!auth.user ? <Redirect to={{pathname: "/"}} /> : <Redirect to={{pathname: "/login"}} /> }
-
-      <Route exact path="/" >
-        <Main t={t.bind(this)} />
-      </Route>
-
-      <Route exact path="/login">
-        <Login />
-      </Route>
-      <Route exact path="/register">
-        <Register />
-      </Route>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path="/" component={ () => <Main t={t.bind(this)} /> } />
+        <AuthRoute exact path="/login" component={ () => <Login /> } />
+        <AuthRoute exact path="/register" component={ () => <Register /> }  />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -5,10 +5,19 @@ export const UserContext = createContext();
 
 // This context provider is passed to any component requiring the context
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(firebase.auth() || null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser);
+    firebase.auth().onAuthStateChanged( user => {
+      if(user) {
+        localStorage.setItem('user', JSON.stringify(user));
+        setUser(user);
+      }
+      else {
+        localStorage.removeItem('user');
+        setUser(null);
+      }
+    });
   }, [] );
 
   return (
