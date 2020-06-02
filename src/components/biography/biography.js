4506de7a5782fb6d01b047749b758a86d66bc0f3
Natan Kruger <natan.kruger@zygotecnologia.com>
2020-06-02 17:38:57 -0300

    Making a loader for image upload

--- src/components/biography/biography.js
+++ src/components/biography/biography.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/core";
 
 import firebase from '../../services/firebase';
 
 class Biography extends React.Component {
   constructor(props) {
     super();
-    this.state = { profilePicUrl: "" }
+    this.state = { profilePicUrl: "", profilePicLoading: false }
   }
 
   componentDidMount() {
     let user = firebase.auth().currentUser;
-    console.log(user);
     this.setState({ profilePicUrl: user.photoURL });
   }
 
   uploadFile(e) {
     let user = firebase.auth().currentUser;
     let storage = firebase.storage().ref().child("user_images").child(user.uid);
+    this.setState({ profilePicLoading: true });
+
     storage.put(e.target.files[0]).then((snapshot) => {
       snapshot.ref.getDownloadURL().then((downloadURL) => {
         user.updateProfile( { photoURL: downloadURL } );
-        this.setState({ profilePicUrl: downloadURL })
+        this.setState({ profilePicUrl: downloadURL, profilePicLoading: false });
       });
     });
 
@@ -30,11 +33,14 @@ class Biography extends React.Component {
   listBio() {
     let t = this.props.t;
     let maxLength = 250;
+    const override = css`display: block; margin: 50px auto 0 auto;`;
 
     return <React.Fragment>
-      <div>
-        { this.props.edition && <input type="file" onChange={ (e) => this.uploadFile(e) } /> }
-        { this.state.profilePicUrl && <img src={ this.state.profilePicUrl } className="profile-pic" alt="Profile Natan face" /> }
+      <div className="profile-pic-wrapper">
+        { this.state.profilePicUrl && this.state.profilePicLoading ? <ClipLoader css={ override } /> : <img src={ this.state.profilePicUrl } className="profile-pic" alt="Profile Natan face" /> }
+      </div>
+      <div className="profile-input">
+        { this.props.edition && <input type="file" accept="image/*" className="mt-2" onChange={ (e) => this.uploadFile(e) } /> }
       </div>
       <div className="mt-3">
         { this.props.edition ? <div className="form-group">
